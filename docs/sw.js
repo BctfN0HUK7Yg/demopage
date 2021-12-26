b3343bb5cec99f4c1212976881c33ab833b546e1
BctfN0HUK7Yg <oksimets@yandex.ru>
2021-12-27 00:25:18 +0300

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'metyolka';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './metyolka.js',
+  './metyolka_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
